7ce2c6f0f4fa37ed004601c6fedfe9b6857e4916
Bits2green <neshmunesh2@gmail.com>
2023-02-16 12:23:50 -0800

    Hello Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program to print a lineusing printf
+ * Retun 0: Seuccess
+ */
+
+int main(void)
+{
+	printf("with proper grnnar, but the outcome is a piece of art, \n")
+	return (0);
+}
