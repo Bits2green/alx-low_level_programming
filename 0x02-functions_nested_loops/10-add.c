4ed760d8e71b5febfa837754def40164e1df87fb
Bits2green <neshmunesh2@gmail.com>
2023-02-21 15:15:06 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ *@a: first member
+ *@b: second member
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
