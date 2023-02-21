cadb72cb2a5581053aafa2f3b94498f1325ecc6c
Bits2green <neshmunesh2@gmail.com>
2023-02-21 06:18:17 -0800

    print 10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int c
+	char lt;
+
+	c = 1
+	while c < 10
+	{
+		for (lt = 'a'; lt <= 'z'; lt++)
+			_putchar(lt);
+		_putchar('\n');
+	}
+}
+
