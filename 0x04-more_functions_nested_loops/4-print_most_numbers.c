32e918f61303e1a513ef7d6454dcdb5741231a92
Bits2green <neshmunesh2@gmail.com>
2023-02-23 13:38:21 -0800

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry
+ *
+ */
+
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (!(x == '2' || x == '4'))
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
