f925469b842f42c8d08dbcdcc3b7ab925489d0df
Bits2green <neshmunesh2@gmail.com>
2023-02-23 22:50:24 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Entry
+ * @n: first member
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j <= n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
