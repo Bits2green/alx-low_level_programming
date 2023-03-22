9bf9808be0f6e5d676936f5b2b5802397d064c05
Bits2green <neshmunesh2@gmail.com>
2023-03-22 09:23:45 -0700

    Print Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_nam - Entry
+ * @name: name given
+ * @f: function of the name
+ *
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	if (f != NULL && f != NULL)
+		f(name)
+}
