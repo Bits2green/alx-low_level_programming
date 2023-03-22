2ea0553e058d83bb58567c80a3db5fb60e8a0c58
Bits2green <neshmunesh2@gmail.com>
2023-03-22 11:48:45 -0700

    Int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,19 +11,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (size > 0)
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (i < size)
-			{
-				if ((*cmp)(array[i]) == 0)
-					return (i);
-				i++;
-			}
-		}
+		if ((*cmp)(array[i]) == 0)
+			return (i);
 	}
 	return (-1);
 }
