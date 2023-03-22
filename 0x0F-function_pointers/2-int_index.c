a54cef74d5a126e3477c7bf4136c5387bb4d65c1
Bits2green <neshmunesh2@gmail.com>
2023-03-22 11:36:05 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Entry
+ * @array: list of elements
+ * @size: size of array
+ * @cmp: function toiterate over the array
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if ((*cmp)(array[i]) == 0)
+					return (i);
+				i++;
+			}
+		}
+	}
+	return -1;
+}
