286c6555230e5584bf5890c1090f35ff86547610
Bits2green <neshmunesh2@gmail.com>
2023-09-05 00:02:15 +0000

    hot fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,30 +1,36 @@
 #include "main.h"
 
 /**
- * copy_file - copies the content of a file to another file.
- * @code:
- * @message:
+ * exit_with_error - checks if the correct number of arguments.
+ * @code: error code 97 or 98
+ * @message: message printed to the standard error
  *
- *Return: 
+ * Return:
  */
 
 void exit_with_error(int code, const char *message)
 {
-    dprintf(STDERR_FILENO, "Error: %s\n", message);
-    exit(code); }
+	dprintf(STDERR_FILENO, "Error: %s\n", message);
+	exit(code); }
 
-int main(int argc, char *argv[]) 
+/**
+ * main - copies the content of one file to another
+ * @argc: args
+ * @argv:
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
 {
-	const char *file_from;
-	const char *file_to;
-	int fileDto;
-	int fileDfrom;
+	const char *file_from, *file_to;
+	int fileDto, fileDfrom;
 	char buffer[BUF_SIZE];
-        ssize_t bytesR;
+	ssize_t bytesR;
 
 	if (argc != 3)
 	{
-	    exit_with_error(97, "Usage: cp file_from file_to"); }
+		exit_with_error(97, "Usage: cp file_from file_to"); }
 	file_from = argv[1];
 	file_to = argv[2];
 	fileDfrom = open(file_from, O_RDONLY);
@@ -32,13 +38,14 @@ int main(int argc, char *argv[])
 	{
 		exit_with_error(98, "Can't read from file"); }
 	fileDto = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fileDto == -1) 
+	if (fileDto == -1)
 	{
 		close(fileDfrom);
 		exit_with_error(99, "Can't write to file"); }
 	while ((bytesR = read(fileDfrom, buffer, BUF_SIZE)) > 0)
 	{
 		ssize_t bytesW = write(fileDto, buffer, bytesR);
+
 		if (bytesW == -1)
 		{
 			close(fileDfrom);
@@ -52,4 +59,4 @@ int main(int argc, char *argv[])
 	if (close(fileDfrom) == -1 || close(fileDto) == -1)
 	{
 		exit_with_error(100, "Can't close file descriptor"); }
-	return 0; }
+	return (0); }
