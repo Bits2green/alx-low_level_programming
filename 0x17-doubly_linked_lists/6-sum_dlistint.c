362aab030fe39ef12bef7253d1db0535db1f9582
Bits2green <neshmunesh2@gmail.com>
2023-11-09 18:47:21 +0000

    fr

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * node_sum_dlistint - node_sums all of the data of a dlistint_t linked list
+ * sum_dlistint - sums all of the data of a dlistint_t linked list
  * @head: pointer to the beginning of the linked list
  *
- * Return: node_sum of all data, or 0 if the list is empty
+ * Return: sum of all data, or 0 if the list is empty
  */
-int node_sum_dlistint(dlistint_t *head)
+int sum_dlistint(dlistint_t *head)
 {
 	int node_sum = 0;
 
