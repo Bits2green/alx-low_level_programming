e7d63347ff410220cebcbe176336faccc7f845a3
Bits2green <neshmunesh2@gmail.com>
2023-11-13 10:45:43 +0300

    Dynamic Libraries

--- 0x18-dynamic_libraries/_memcpy.c
+++ 0x18-dynamic_libraries/_memcpy.c
@@ -11,12 +11,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int a;
-a = 0;
+    unsigned int a;
+    a = 0;
 
-while (a < n)
-{
-dest[a] = src[a];
-a++;
-}
+    while (a < n)
+    {
+        dest[a] = src[a];
+        a++;
+    }
+    return (0);
 }
