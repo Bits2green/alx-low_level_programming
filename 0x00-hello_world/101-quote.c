5bbedcdb986c9bb37a5b010e3d455a58b25e07c6
root <root@DESKTOP-O6NJFJ6.>
2025-03-19 19:26:20 +0300

    write()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 	const char *message;
-       	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	write(2,message,59);
+
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
 	return (0);
 }
