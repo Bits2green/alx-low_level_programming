383174b46d6faa7f20873fb19721916c62774df6
root <root@DESKTOP-O6NJFJ6.>
2025-04-02 19:57:15 +0300

    prints the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Entry Point
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  * Return: Always success (0)
- *
  */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
 }
+
