df4cf687857a8278c2400bf41994d4806153c0c7
root <root@DESKTOP-O6NJFJ6.>
2025-04-02 18:00:33 +0300

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * _putchar - prints '_putchar'
+ * Return: Always Success (0)
+ */
+
+char _putchar(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always Success (0)
+ */
+
+int main(void)
+{
+	_putchar();
+	return (0);
+}
+
