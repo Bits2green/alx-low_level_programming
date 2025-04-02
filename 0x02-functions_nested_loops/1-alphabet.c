ff55be5cd448bb2c82a08f1c1badc0532e00bb95
root <root@DESKTOP-O6NJFJ6.>
2025-04-02 19:43:50 +0300

    function that prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Return: Always success (0)
+ *
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -10,3 +10,21 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * Return: characters of the alphabet on success, -1 on error.
+ */
+
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
