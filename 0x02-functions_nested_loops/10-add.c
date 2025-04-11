14405537c84bd2f98279cc63de744336b9ac4254
root <root@DESKTOP-O6NJFJ6.>
2025-04-11 18:28:26 +0300

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result on success.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
