d0a4880a2fd96f51c39d91c5fdcf9d535f8c8847
root <root@DESKTOP-O6NJFJ6.>
2025-04-11 18:14:47 +0300

    prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * prints the 9 times table, starting with 0.
+ * Return: the 9 times table on success.
+ */
+
+void times_table(void)
+{
+	int row = 0;
+	int col;
+	int prod;
+	
+	/* Handle rows 0-9 */
+	while (row <= 9)
+	{
+		col = 0;
+		while (col <= 9)
+		{
+			prod = row * col;
+			
+			/* Handle first column without comma */
+			if (col == 0)
+			{
+				if (prod < 10)
+					_putchar(prod + '0');
+			}
+			else
+			{
+				/* Add comma and space */
+				_putchar(',');
+				_putchar(' ');
+
+				if (prod < 10)
+					_putchar(' ');
+				
+				/* Handle larger than 10 numbers */
+				if (prod >= 10)
+					_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			col++;
+		}
+		_putchar('$');
+		_putchar('\n');
+		row++;
+	}
+
+
